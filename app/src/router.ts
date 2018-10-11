2caf78756e66cb02399050bda0a411fbc4e15c7f
Dylan Brown <dylanbrown@uvic.ca>
2018-10-11 15:04:42 -0700

    Add Login functionality as much as possible

--- app/src/router.ts
+++ app/src/router.ts
@@ -1,10 +1,14 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Login from './views/Login.vue';
+import User from './views/User.vue';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
@@ -12,12 +16,14 @@ export default new Router({
       component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/auth/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/user/:userId/home',
+      name: 'userhome',
+      component: User,
     },
   ],
 });
